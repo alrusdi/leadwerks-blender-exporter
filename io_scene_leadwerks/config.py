ddb8bb2504a02b85df61861d6448da19ba3a1abf
alrusdi <alrusdi@gmail.com>
2014-05-30 09:52:32 +0600

    Added missing configuration module

---
+++ io_scene_leadwerks/config.py
@@ -0,0 +1,34 @@
+# <pep8 compliant>
+
+
+class CONFIG(object):
+    """
+    This values are managed by user from GUI
+    """
+    file_version = 2
+    file_extension = '.mdl'
+    export_selection = False
+    export_animation = True
+    export_materials = True
+    overwrite_textures = False
+    export_specular_color = False
+    write_debug_xml = True
+
+    @classmethod
+    def update(cls, options):
+        for k, v in cls.values().items():
+            default = v
+            val = options.get(k, default)
+            setattr(cls, k, val)
+        if cls.file_extension == '.gmf':
+            cls.file_version = 1
+
+    @classmethod
+    def values(cls):
+        vals = {}
+        types = [bool, str, int, float, dict]
+        for k, v in cls.__dict__.items():
+            if k.startswith('_') or type(v) not in types:
+                continue
+            vals[k] = v
+        return vals
